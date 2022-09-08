a9f1f7e1e9a9113639a0ef761ebe7827902187f1
Eurico Moutinho <yungsniper@hotmail>
2022-09-08 15:40:50 +0100

    update

--- up-work-client/src/components/AddJobs.js
+++ up-work-client/src/components/AddJobs.js
@@ -1,18 +1,75 @@
-import React from 'react'
+import { useState } from "react";
+import axios from "axios";
 
-function AddJobs() {
+const API_URL = "http://localhost:5005";
+
+function AddJobs(props) {
+
+    const [title, setTitle] = useState("");
+    const [company, setCompany] = useState("");
+    const [description, setDescription] = useState("");
+    const [skills, setSkills] = useState("");
+    const [level, setLevel] = useState("");
+    const [owner, setOwner] = useState("");
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const requestBody = { title, description, skills, level };
+    
+    const storedToken = localStorage.getItem('authToken');
+ 
+    axios
+      .post(
+      `${API_URL}`,
+      requestBody,
+      { headers: { Authorization: `Bearer ${storedToken}` } } 
+    )
+      .then((response) => {
+      // Reset the state
+      setTitle("");
+      setCompany("");
+      setDescription("");
+      setSkills("");
+      setLevel("");
+      setOwner("");
+      
+    })
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <div>
+    <div >
         <h1>Add Job</h1>
+        <form onSubmit={handleSubmit}>
+
         <label>Title</label>
-        <input type='text'/>
+        <input type="text"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}/>
 
         <label>Description:</label>
-        <textarea type="text" name=""></textarea>
+        <textarea type="text"
+          name="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}></textarea>
 
-        <label>Contact: </label>
-        <input type='text'/>
+        <label>Skills: </label>
+        <input type="text"
+          name="skills"
+          value={skills}
+          onChange={(e) => setSkills(e.target.value)}/>
+
+        <label>Level: </label>
+        <input type="text"
+          name="level"
+          value={level}
+          onChange={(e) => setLevel(e.target.value)}/>
+
         <button type="submit">Add Job</button>
+        </form>
     </div>
   )
 }
--- up-work-client/src/components/editJob.js
+++ up-work-client/src/components/editJob.js
@@ -1,20 +1,93 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from 'react-router-dom';
+import axios from "axios";
 
-function editJob() {
+const API_URL = "http://localhost:5005";
+
+function EditJob(props) {
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [skills, setSkills] = useState("");
+    const [level, setLevel] = useState("");
+  
+  const { jobId } = useParams();
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const storedToken = localStorage.getItem('authToken');
+    axios
+      .get(`${API_URL}/${jobId}`, { headers: { Authorization: `Bearer ${storedToken}` } })
+      .then((response) => {
+        const oneJob = response.data;
+        setTitle(oneJob.title);
+        setDescription(oneJob.description);
+        setSkills(oneJob.skills);
+        setLevel(oneJob.level);
+
+      })
+      .catch((error) => console.log(error));
+    
+  }, [jobId]);
+  
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = { title, description, skills, level };
+    const storedToken = localStorage.getItem('authToken');
+
+    axios
+      .put(`${API_URL}/${jobId}`, requestBody, { headers: { Authorization: `Bearer ${storedToken}` } })
+      .then((response) => {
+        navigate(`/jobs/${jobId}`)
+      });
+  };
+  
+  
+  const deleteJob = () => {
+    const storedToken = localStorage.getItem('authToken');
+    axios
+      .delete(`${API_URL}/${jobId}`, { headers: { Authorization: `Bearer ${storedToken}` } })
+      .then(() => {
+        navigate("/jobs");
+      })
+      .catch((err) => console.log(err));
+  };  
+
   return (
     <div>
         <h1>Edit Job</h1>
+        <form  onSubmit={handleFormSubmit}>
+
         <label>Title</label>
-        <input type='text'/>
+        <input type="text"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}/>
 
         <label>Description:</label>
-        <textarea type="text" name=""></textarea>
+        <textarea type="text"
+          name="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}></textarea>
 
-        <label>Contact: </label>
-        <input type='text'/>
-        <button type="submit">Edit Job</button>
+        <label>Skills: </label>
+        <input type="text"
+          name="skills"
+          value={skills}
+          onChange={(e) => setSkills(e.target.value)}/>
+
+        <label>Level: </label>
+        <input type="text"
+          name="level"
+          value={level}
+          onChange={(e) => setLevel(e.target.value)}/>
+
+        <button type="submit">Add Job</button>
+        </form>
+        <button onClick={deleteJob}>Delete Job</button>
     </div>
   )
 }
 
-export default editJob
+export default EditJob
