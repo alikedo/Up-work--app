5872b5bab6bca5fcae28026cf1930a1eb84ff2a9
Eurico Moutinho <yungsniper@hotmail>
2022-09-09 11:11:47 +0100

    update

--- up-work-client/src/index.js
+++ up-work-client/src/index.js
@@ -1,17 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProviderWrapper } from "./context/auth.context";
 
-ReactDOM.render(
+const container = document.getElementById("root")
+const root =createRoot(container);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
     <AuthProviderWrapper>
         <App />
       </AuthProviderWrapper>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
