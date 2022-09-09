c723d7525cbd6f8601e274da8f70e2fb48689b0c
Eurico Moutinho <yungsniper@hotmail>
2022-09-09 13:55:15 +0100

    update

--- up-work-client/src/index.js
+++ up-work-client/src/index.js
@@ -9,7 +9,8 @@ import { AuthProviderWrapper } from "./context/auth.context";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <AuthProviderWrapper>
@@ -20,7 +21,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
